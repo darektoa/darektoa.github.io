8de336394bd1ad833bc9a95133a63f6e387890f2
darektoa <darektoa@gmail.com>
2020-07-14 00:41:49 +0700

    first commit

---
+++ push.js
@@ -0,0 +1,34 @@
+const webPush = require('web-push');
+ 
+const vapidKeys = {
+    "publicKey"  : "BNLhJiri3FKB6EI10AvVTYg5aOQjlh5aE7uKIVAr_V5v7zXxOIvE4IarMs_IXPtCFGWVdiWoAN7ljP4jWWkV0f0",
+    "privateKey" : "6q0iQb8V8_6YoinwivmRFlz7yiLfneHJ1-Shj5wY0dA"
+};
+ 
+ 
+webPush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
+const pushSubscription = {
+    "endpoint"  : "https://fcm.googleapis.com/fcm/send/d-B56b2ilzY:APA91bHH1e-MumNFkN0HKxGkynz1JowbFlu7GK-dcSpoDo4Nj8yhFef-PBpdmXA8zyTvOmVXjI2PQ1dbzpDrtkDDgq1S06KbWBj3sFVw6QWgi_NWc7bp5_r71zLSYK0XbpRgrrakSuAA",
+    "keys"      : {
+        "p256dh": "BJMosbkM8WpdS4GK3vJHbhD2C9LlQXvTaVW3S87+3535akrHcK30VZLTweHZ/s0ojSa35Gw0+CoWUO/uv2IVS/4=",
+        "auth"  : "fUCHmLmdXgHYTlX9ad/Qdw=="
+    }
+};
+
+const payload = "Periksa sekarang! Ada notifikasi baru dari Rexball!";
+ 
+const options = {
+    gcmAPIKey: '151030493738',
+    TTL: 60
+};
+
+webPush.sendNotification(
+    pushSubscription,
+    payload,
+    options
+);
---
+++ service-worker.js
@@ -0,0 +1,104 @@
+const CACHE_NAME = 'v1.0';
+
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/src/html/nav.html',
+    '/src/images/icons/72x72.png',
+    '/src/images/icons/192x192.png',
+    '/src/images/icons/512x512.png',
+    '/src/styles/materialize.min.css',
+    '/src/styles/style.css',
+    '/src/scripts/materialize.min.js',
+    '/src/scripts/manipulation.js',
+    '/src/scripts/req-notif.js',
+    '/src/scripts/regis-sw.js',
+    '/src/scripts/nav.js'
+]
+
+self.addEventListener('install', event=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache =>{
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+self.addEventListener('fetch', event=>{
+    const request = toSecureRequest(event.request);
+
+    event.respondWith(
+        caches.match(request)
+        .then(response =>{
+            return response || fetch(request)
+            .then(resFetch =>{
+                if(resFetch.ok){
+                    caches.open(CACHE_NAME)
+                    .then(cache =>{
+                        cache.put(request.url, resFetch);
+                    })
+                }
+                return resFetch.clone();
+            });
+        })
+    )
+})
+
+self.addEventListener('activate', event=>{
+    self.clients.claim();
+
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames =>{
+            return Promise.all(
+                cacheNames.map(cacheName =>{
+                    if(cacheName != CACHE_NAME)
+                        return caches.delete(cacheName);
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener('push', event=>{
+    let body;
+    
+    if(event.data)
+        body = event.data.text();
+    else
+        body = 'Push message no payload';
+    
+    const options = {
+        body    : body,
+        icon    : '/src/images/icons/192x192.png',
+        vibrate : [200, 100, 200],
+        data    : {
+            dateOfArrival : Date.now(),
+            primaryKey    : 1
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Rexball Notification', options)
+    );
+});
+
+const toSecureRequest = request=>{
+    if(request.url.match(location.origin)) return request;
+
+    const url  = request.url.replace('http://', 'https://');
+    const init = {};
+
+    for(item in request){
+        if(item === 'url') continue;
+        if(typeof(request[item]) === 'function') break;
+
+        init[item] = request[item];
+    }
+
+    const newRequest = new Request(url, init);
+
+    return newRequest;
+}
