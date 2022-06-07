c95378135b1260fffb9f1bc8b731d29fb98d68c6
darektoa <darektoa@gmail.com>
2022-06-08 05:03:55 +0700

    Add: Shadow-gradient-radial-circle class in theme tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        grid: ["url('../assets/images/grids.png')"]
+        grid: ["url('../assets/images/grids.png')"],
+        'gradient-radial-circle': 'radial-gradient(circle, var(--tw-gradient-stops))'
       },
       boxShadow: {
         sunny: '0 4px 30px 0 rgba(0, 0 ,0, 0.25)'
