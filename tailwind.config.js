990ca620a40df7f48ccd88f4dcf9297c40237586
darektoa <darektoa@gmail.com>
2022-06-08 05:02:29 +0700

    Add: Shadow-sunny class in theme tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         grid: ["url('../assets/images/grids.png')"]
+      },
+      boxShadow: {
+        sunny: '0 4px 30px 0 rgba(0, 0 ,0, 0.25)'
       }
     },
   },
