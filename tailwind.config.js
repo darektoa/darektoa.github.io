0103b0b5cd4583d219d93585514c6bc5b046c8b1
darektoa <darektoa@gmail.com>
2022-06-11 05:03:19 +0700

    Add: background image wave class in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         grid: ["url('../assets/images/grids.png')"],
-        'gradient-radial-circle': 'radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))'
+        'gradient-radial-circle': 'radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))',
+        wave: "url('../assets/images/wave.svg')"
       },
       boxShadow: {
         sunny: '0 4px 30px 0 rgba(0, 0 ,0, 0.25)'
